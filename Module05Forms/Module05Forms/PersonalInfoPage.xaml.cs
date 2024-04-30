9cc56e9d2e93e6039923644e0d7198a376f57caa
ADMIN <ADMIN@DESKTOP-EQUAN2O>
2024-05-01 02:28:11 +0800

    Add project files.

---
+++ Module05Forms/Module05Forms/PersonalInfoPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module05Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalInfoPage : ContentPage
+    {
+        public PersonalInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnDateSelected(object sender, DateChangedEventArgs args)
+        {
+            DateTime birthdate = birthDatePicker.Date;
+
+            DateTime today = DateTime.Today;
+
+            int age = today.Year - birthdate.Year;
+
+            result.Text = age.ToString();
+        }
+    }
+}
